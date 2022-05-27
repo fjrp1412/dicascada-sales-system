d2d6d908604c6961dbd709a24b176e89c57c0250
Francisco Ruiz <fruiz.rc@gmail.com>
2022-05-27 00:21:27 -0400

    remove icons from navbar

--- src/components/dashboard-navbar.js
+++ src/components/dashboard-navbar.js
@@ -48,32 +48,7 @@ export const DashboardNavbar = (props) => {
           </IconButton>
 
           <Box sx={{ flexGrow: 1 }} />
-          <Tooltip title="Contacts">
-            <IconButton sx={{ ml: 1 }}>
-              <UsersIcon fontSize="small" />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Notifications">
-            <IconButton sx={{ ml: 1 }}>
-              <Badge
-                badgeContent={4}
-                color="primary"
-                variant="dot"
-              >
-                <BellIcon fontSize="small" />
-              </Badge>
-            </IconButton>
-          </Tooltip>
-          <Avatar
-            sx={{
-              height: 40,
-              width: 40,
-              ml: 1
-            }}
-            src="/static/images/avatars/avatar_1.png"
-          >
-            <UserCircleIcon fontSize="small" />
-          </Avatar>
+
         </Toolbar>
       </DashboardNavbarRoot>
     </>
